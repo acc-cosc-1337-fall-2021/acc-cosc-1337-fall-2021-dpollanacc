3b3d492be9fe40db2f4d0cf711b37884c14bc14b
dpollanacc <david.pollan@g.austincc.edu>
2021-10-01 07:21:34 -0500

    Implement factorial and gcd functions

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,22 @@
 //add include statements
 
 //add function(s) code here
+int factorial(int num) {
+    int sum = 1;
+    while (num > 0) {
+        sum *= num--;
+    }
+    return sum;
+}
+
+int gcd(int num1, int num2) {
+    while (num1 != num2) {
+        if (num1 < num2) {
+            int swap = num1;
+            num1 = num2;
+            num2 = swap;
+        }
+        num1 -= num2;
+    }
+    return num1;
+}
